ab292ae9e8ed783ff5cd949270486eabd66bdff9
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-25 14:25:54 +0200

    FUCK YOU

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -80,13 +80,7 @@ class MusicBeatState extends FlxUIState
 
                 if (PlayState.SONG.dodgeEnabled)
                 {
-                        if (androidc.mode == HITBOX) {
-                                _virtualpad = new FlxVirtualPad(NONE, D);
-                        } else if (androidc.mode == KEYBOARD) {
-                                _virtualpad = new FlxVirtualPad(NONE, NONE);
-                        } else {
-                                _virtualpad = new FlxVirtualPad(NONE, A);
-                        }
+                        _virtualpad = new FlxVirtualPad(NONE, A);
 		        _virtualpad.alpha = 0.75;
                         _virtualpad.cameras = [camcontrol];
                         _virtualpad.visible = false;
